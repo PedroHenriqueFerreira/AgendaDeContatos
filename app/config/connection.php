2fcaef274b52cb24b7df7d19036a88849b9a54a2
Pedro Henrique Ferreira <63063916+PedroHenriqueFerreira@users.noreply.github.com>
2022-11-18 16:33:30 -0300

    Update connection.php

--- app/config/connection.php
+++ app/config/connection.php
@@ -11,7 +11,7 @@ class Connection
 
       return new PDO('mysql:host=' . $host . ';dbname=' . $db, $user, $password);
     } catch (Exception $e) {
-      return die('Database error');
+      return die('Houve algum erro no banco de dados');
     }
   }
 }
